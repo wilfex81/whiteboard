b6f7718328e240d51be688f092d186f2fbd39e0d
wilfex81 <84175893+wilfex81@users.noreply.github.com>
2022-06-07 22:20:35 +0300

    Update pigLatin.py

--- pigLatin.py
+++ pigLatin.py
@@ -1,4 +1,7 @@
 #English to Pig Latin
+#Pig Latin is a silly made-up language that alters English words. If a word begins with a vowel, the word yay is added to the end of it. 
+#If a word begins with a consonant or consonant cluster (like ch or gr), that consonant or cluster is moved to the end of the word followed by ay
+
 print( "\n" 'Enter an English Message to transalte into Pig Latin: ')
 
 message = input()
